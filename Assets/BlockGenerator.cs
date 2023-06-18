18a80e612d13ada585ff7dcccf958d941011f42e
Kahoneki <shinyzorua6@gmail.com>
2023-06-18 20:21:47 +0100

    Alpha v2.0.1 - Fixed lighting on voxels. 	modified:   Assets/BlockGenerator.cs

--- Assets/BlockGenerator.cs
+++ Assets/BlockGenerator.cs
@@ -36,16 +36,41 @@ public class BlockGenerator : MonoBehaviour
         vertices = new Vector3[]
         {
             //y- vertices
-            new Vector3(-.5f, -.5f, -.5f),
-            new Vector3(-.5f, -.5f, .5f),
-            new Vector3(.5f, -.5f, -.5f),
-            new Vector3(.5f, -.5f, .5f),
+            new Vector3(.5f, -.5f, .5f),    //0
+            new Vector3(-.5f, -.5f, .5f),   //1
+            new Vector3(-.5f, -.5f, -.5f),  //2
+            new Vector3(.5f, -.5f, -.5f),   //3
+            
 
             //y+ vertices
-            new Vector3(-.5f, .5f, -.5f),
-            new Vector3(-.5f, .5f, .5f),
-            new Vector3(.5f, .5f, -.5f),
-            new Vector3(.5f, .5f, .5f)
+            new Vector3(.5f, .5f, -.5f),    //4
+            new Vector3(-.5f, .5f, -.5f),   //5
+            new Vector3(-.5f, .5f, .5f),    //6
+            new Vector3(.5f, .5f, .5f),     //7
+
+            //x- vertices
+            new Vector3(-.5f, -.5f, -.5f),  //8
+            new Vector3(-.5f, -.5f, .5f),   //9
+            new Vector3(-.5f, .5f, .5f),    //10
+            new Vector3(-.5f, .5f, -.5f),   //11
+
+            //x+ vertices
+            new Vector3(.5f, -.5f, .5f),    //12
+            new Vector3(.5f, -.5f, -.5f),   //13
+            new Vector3(.5f, .5f, -.5f),    //14
+            new Vector3(.5f, .5f, .5f),     //15
+
+            //z- vertices
+            new Vector3(.5f, -.5f, -.5f),   //16
+            new Vector3(-.5f, -.5f, -.5f),  //17
+            new Vector3(-.5f, .5f, -.5f),   //18
+            new Vector3(.5f, .5f, -.5f),    //19
+
+            //z+ vertices
+            new Vector3(-.5f, -.5f, .5f),   //20
+            new Vector3(.5f, -.5f, .5f),    //21
+            new Vector3(.5f, .5f, .5f),     //22
+            new Vector3(-.5f, .5f, .5f)     //23
         };
 
         triangles = new List<int>();
@@ -54,27 +79,27 @@ public class BlockGenerator : MonoBehaviour
         foreach (Vector3 face in facesToShow)
         {
             if (face == offsets[0]) {
-                int[] currentFace = {3,7,1,7,5,1};
+                int[] currentFace = {20,21,22,22,23,20};
                 triangles.AddRange(currentFace);
             }
             else if (face == offsets[1]) {
-                int[] currentFace = {0,4,2,4,6,2};
+                int[] currentFace = {16,17,18,18,19,16};
                 triangles.AddRange(currentFace);
             }
             else if (face == offsets[2]) {
-                int[] currentFace = {2,6,3,6,7,3};
+                int[] currentFace = {12,13,14,14,15,12};
                 triangles.AddRange(currentFace);
             }
             else if (face == offsets[3]) {
-                int[] currentFace = {1,5,0,5,4,0};
+                int[] currentFace = {8,9,10,10,11,8};
                 triangles.AddRange(currentFace);
             }
             else if (face == offsets[4]) {
-                int[] currentFace = {4,5,6,5,7,6};
+                int[] currentFace = {4,5,6,6,7,4};
                 triangles.AddRange(currentFace);
             }
             else if (face == offsets[5]) {
-                int[] currentFace = {2,1,0,2,3,1};
+                int[] currentFace = {0,1,2,2,3,0};
                 triangles.AddRange(currentFace);
             }
         }
