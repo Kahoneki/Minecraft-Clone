5f3233b440a7c2d08c019955415d14d04e9b7788
Kahoneki <shinyzorua6@gmail.com>
2023-06-28 23:57:34 +0100

    Alpha v4.0.1 - Started work on biome generation. 	new file:   Assets/Scripts/BiomeGenerator.cs 	new file:   Assets/Scripts/BiomeGenerator.cs.meta

---
+++ Assets/Scripts/BiomeGenerator.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BiomeGenerator : MonoBehaviour
+{
+
+    //Biome types and parameters
+    public Dictionary<string, int> amplitude = new Dictionary<string, int> {
+        {"Plains", 8},
+        {"Mountains", 20}
+    };
+
+    
+
+}
